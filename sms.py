439c06e8e7f89069ed144c9bb69e82ffba814559
NightLight <night.light.code@protonmail.ch>
2021-09-02 22:08:39 +0300

    Services Stabled
    New Services stabled and added to sms py file

--- sms.py
+++ sms.py
@@ -10,15 +10,30 @@ import time
 
 import config
 
+import os
+import sys
 
-'''
+
 def run_sms(phone, userid):
+
+    os.system("python3 aptekaapril.py " + phone)
+
+    os.system("python3 citymobil.py " + phone)
+
+    os.system("python3 kari.py " + phone)
+
+    os.system("python3 mvideo.py " + phone)
+
+    os.system("python3 perekrestok.py " + phone)
+
+    os.system("python3 sravni.py " + phone)
+
+    os.system("python3 uber.py " + phone)
+
+
 
 
 if not utilities.is_attack_running(userid):
     bot = telebot.TeleBot(config.bot_token)
     bot.send_message(userid, "СМС остановлены!")
     return 0
-
-
-'''
